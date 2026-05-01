e7fe9d4c9ab36f6c06ef6f26b69f16032924072e
xyc-yz <xty0598@163.com>
2026-05-01 15:36:27 +0800

    代码

--- src/main/java/com/love/loveagentxyc/controller/HealthController.java
+++ src/main/java/com/love/loveagentxyc/controller/HealthController.java
@@ -1,13 +1,26 @@
 package com.love.loveagentxyc.controller;
 
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class HealthController {
+    @Resource
+    private ChatClient chatClient;
     @GetMapping("/health")
     public String health() {
         return "ok，ok";
+    }
+
+    @RequestMapping("/chat")
+    public Flux<String> index() {
+        return chatClient.prompt()
+                .user("你好,你是谁")
+                .stream()
+                .content();
     }
 }
